b5a8a363897ad76e560a21ec849fc4667aa548ef
Ben Wilhelm <zorba-git-osx@pavlovian.net>
2009-12-23 16:00:01 -0800

    make system work on osx

--- ursalib.c
+++ ursalib.c
@@ -7,34 +7,34 @@
 #include <unistd.h>
 
 static int ursalib_system(lua_State *L) {
-  //printf("ULS 1\n");
-  FILE *fil = popen(luaL_checkstring(L, 1), "rb");
-  //printf("ULS 2\n");
+  
+  FILE *fil = popen(luaL_checkstring(L, 1), "r");
+  if(!fil) {
+    luaL_error(L, "failure to popen, world collapsing in flames");
+  }
+
   luaL_Buffer b;
   luaL_buffinit(L, &b);
-  //printf("ULS 3\n");
+
   while(1) {
     char *buff = luaL_prepbuffer(&b);
-    //printf("ULS 4\n");
+
     int siz = fread(buff, 1, LUAL_BUFFERSIZE, fil);
-    //printf("ULS 5\n");
     luaL_addsize(&b, siz);
-    //printf("ULS 6\n");
+    
     if(siz != LUAL_BUFFERSIZE)
       break;
   }
-  //printf("ULS 7\n");
+  
   luaL_pushresult(&b);
-  //printf("ULS 8\n");
   lua_pushinteger(L, pclose(fil));
-  //printf("ULS 9\n");
 
   return 2;
 }
 
 static int ursalib_getcwd(lua_State *L) {
   char bf[2048];
-  if(getcwd(bf, sizeof(bf)) != bf) {
+  if(!getcwd(bf, sizeof(bf))) {
     luaL_error(L, "couldn't get cwd for some wacky reason");
   }
   lua_pushstring(L, bf);
