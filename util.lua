ca485bd6251ab8273d53ad37907d256acd67eb69
Ben Wilhelm <zorba-github@pavlovian.net>
2010-03-07 19:31:51 -0800

    reorganize stuff, make the embed code work properly again maybe hopefully

--- util.lua
+++ util.lua
@@ -70,11 +70,16 @@ function ursa.util.system_template(st)
   end, depends = {"!" .. str, tokz}}
 end
 
+function ursa.util.copy()
+  return ursa.util.system_template{"cp $SOURCE $TARGET"}  -- this may be reimplemented later
+end
+
 local params = {
   system = {1},
   token_deferred = {1, default = true},
   system_template = {1},
   clean = {0}, -- I'm actually not sure how this even works
+  copy = {0},
 }
 
 ursa.gen.wrap_funcs(ursa.util, params)
