9c71e217bed33a72a3ce2624ccec4f17dc8b06d9
Ben Wilhelm <zorba-github@pavlovian.net>
2010-04-17 04:09:37 -0700

    rejigger the context stack a bit

--- ursalibcontext.lua
+++ ursalibcontext.lua
@@ -5,24 +5,25 @@ local ul = {}
 ursalibcontext = ul
 
 -- it may seem like we should be chdir'ing when things are pushed or popped, but we actually live "natively" in the root directory. We only chdir when we need to.
-local context_stack = {{prefix = "", absolute = lib.getcwd()}}
+local context_base = {prefix = "", absolute = lib.getcwd()}
+local context_top = {current = context_base}
 function ul.context_stack_push(chunk)
   --print("pushing stack", chunk.prefix, chunk.absolute)
-  table.insert(context_stack, chunk)
+  context_top = {current = chunk, up = context_top}
 end
 function ul.context_stack_pop(chunk)
   --print("popping stack")
-  assert(context_stack[#context_stack] == chunk)
-  table.remove(context_stack)
+  assert(context_top.current == chunk)
+  context_top = context_top.up
 end
 function ul.context_stack_prefix()
-  if context_stack[#context_stack].prefix == "" then return "" end
-  return context_stack[#context_stack].prefix .. "/"
+  if context_top.current.prefix == "" then return "" end
+  return context_top.current.prefix .. "/"
 end
 function ul.context_stack_chdir(func, ...)
   local cops = lib.getcwd()
-  lib.chdir(context_stack[#context_stack].absolute)
-  current_print_prefix = context_stack[#context_stack].prefix
+  lib.chdir(context_top.current.absolute)
+  current_print_prefix = context_top.current.prefix
   --print("CHDIR INTO", context_stack[#context_stack].absolute)
   local rp = lib.return_pack(func(...))
   current_print_prefix = ""
@@ -31,11 +32,18 @@ function ul.context_stack_chdir(func, ...)
   return lib.return_unpack(rp)
 end
 function ul.context_stack_chdir_native(...)
-  ul.context_stack_push(context_stack[1])
+  ul.context_stack_push(context_base)
   local rp = lib.return_pack(ul.context_stack_chdir(...))
-  ul.context_stack_pop(context_stack[1])
+  ul.context_stack_pop(context_base)
   return lib.return_unpack(rp)
 end
 function ul.context_stack_get()
-  return context_stack[#context_stack]
+  return context_top.current
+end
+
+function ul.context_stack_snapshot_get()
+  return context_top
+end
+function ul.context_stack_snapshot_put(top)
+  context_top = top
 end
